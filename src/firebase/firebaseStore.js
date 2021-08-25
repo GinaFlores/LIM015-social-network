352c9c8fb8d4ca728525f57e83c2a5b38675585f
KeylaMia <keylamialujimenezg@gmail.com>
2021-08-24 23:50:42 -0500

    añadiendo fireStore

--- src/firebase/firebaseStore.js
+++ src/firebase/firebaseStore.js
@@ -1,8 +1,9 @@
-// eslint-disable-next-line no-unused-vars
-const db = firebase.firestore();
-// para hacer referencia a una coleccion
-// eslint-disable-next-line no-unused-vars
-const posts = firebase.firestore().collection('posts');
-// Para obtener todos los datos del documento de una colección .get() devuelve una promesa
-// Para una consulta de colección Podemos acceder a ellos diciendo . snapshot.docs
-//De cada documento, podemos obtener el id como una propiedad separada, y el resto de los datos usando .data()
+// añadiendo documentos a nuestra coleccion de firestore llamadas posts
+export const postCollection = (email, nombre, id, post, photo) => firebase.firestore().collection('posts').add({
+  Email: email,
+  User: nombre,
+  Id: id,
+  Text: post,
+  Photo: photo,
+  timePost: new Date(),
+});
